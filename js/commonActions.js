ba3b64b108db984ec9f9800684c8e74fe9737bf9
Darren Kelly <darrenkelly@fb.com>
2020-09-04 20:53:50 +0100

    added stylez and homepage updates

---
+++ js/commonActions.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+
+    $(".navShowHide").on("click", function() {
+        let main = $("#mainSectionContainer");
+        let nav = $("#sideNavContainer");
+
+        if (main.hasClass("leftPadding")) {
+            nav.hide();
+        } else {
+            nav.show();
+        }
+
+        main.toggleClass("leftPadding");
+
+    });
+
+});
